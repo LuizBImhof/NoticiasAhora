6af7f137ebfc262e38e33df7ec834a07dbc99cd6
LuizImhof <luiz.imhof@unidavi.edu.br>
2019-12-11 00:26:45 -0300

    Colors - nigth mode

--- app/src/main/java/cl/ucn/disc/dsm/news/MainApplication.java
+++ app/src/main/java/cl/ucn/disc/dsm/news/MainApplication.java
@@ -19,6 +19,8 @@ package cl.ucn.disc.dsm.news;
 import android.app.Application;
 import android.content.Context;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 import com.facebook.drawee.backends.pipeline.Fresco;
 
 import org.acra.ACRA;
@@ -49,6 +51,9 @@ public final class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         log.atDebug().log("Initializing Fresco");
         Fresco.initialize(this);
         log.atDebug().log("... Fresco initialized");
